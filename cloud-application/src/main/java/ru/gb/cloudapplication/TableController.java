e5929491ede7e9cdcbc81136c9c64109ce084184
NikitaLubimov <ikarus11122@gmail.com>
2022-05-28 18:25:58 +0300

    работа с раб компа

--- cloud-application/src/main/java/ru/gb/cloudapplication/TableController.java
+++ cloud-application/src/main/java/ru/gb/cloudapplication/TableController.java
@@ -2,6 +2,7 @@ package ru.gb.cloudapplication;
 
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.beans.property.SimpleStringProperty;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
@@ -9,9 +10,7 @@ import javafx.scene.layout.VBox;
 
 import java.io.IOException;
 import java.net.URL;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.nio.file.*;
 import java.util.ResourceBundle;
 import java.util.stream.Collectors;
 
@@ -19,8 +18,13 @@ public class TableController implements Initializable {
 
     @FXML
     public TableView<FileInfo> tableViewClient;
+
     @FXML
     public VBox cloudViewClient;
+    @FXML
+    public ComboBox<String> diskBox;
+    @FXML
+    public TextField pathField;
 
 
     @Override
@@ -60,17 +64,31 @@ public class TableController implements Initializable {
         tableViewClient.getColumns().addAll(fileTypeColumn, fileNameColumn, fileSizeColumn);
         tableViewClient.getSortOrder().add(fileTypeColumn);
 
+        diskBox.getItems().clear();
+        for (Path p: FileSystems.getDefault().getRootDirectories()) {
+            diskBox.getItems().add(p.toString());
+        }
+        diskBox.getSelectionModel().select(0);
+
         updateList(Paths.get("."));
     }
 
     public void updateList (Path path) {
         try {
+            pathField.setText(path.normalize().toAbsolutePath().toString());
             tableViewClient.getItems().clear();
             tableViewClient.getItems().addAll(Files.list(path).map(FileInfo::new).collect(Collectors.toList()));
             tableViewClient.sort();
         } catch (IOException e) {
             Alert alert = new Alert(Alert.AlertType.WARNING, "Не удалось обновить список файлов", ButtonType.OK);
             alert.showAndWait();
+        }
+    }
+
+    public void upPath(ActionEvent actionEvent) {
+        Path upPath = Paths.get(pathField.getText()).getParent();
+        if (upPath != null) {
+            updateList(upPath);
         }
     }
 }
